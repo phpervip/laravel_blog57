c795bc564e63611fa23499e42e8618adee4cf7e3
phper <2998658517@qq.com>
2019-08-25 04:07:02 +0000

    mail.php增加stream

--- config/mail.php
+++ config/mail.php
@@ -135,10 +135,12 @@ return [
 
 
     'stream' => [
-    'ssl' => [
-        'allow_self_signed' => true,
-        'verify_peer' => false,
-        'verify_peer_name' => false,
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ]
+
     ],
 
 
